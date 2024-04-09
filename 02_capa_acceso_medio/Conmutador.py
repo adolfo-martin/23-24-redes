3312fb479432e5df66c4b38d1f845b9f0fdc4e45
adolfo-martin <adolfo.martin@iesramonarcas.es>
2024-04-09 10:05:14 +0200

    hoy

--- 02_capa_acceso_medio/Conmutador.py
+++ 02_capa_acceso_medio/Conmutador.py
@@ -2,6 +2,44 @@ from PuertoRed import PuertoRed
 
 
 class Conmutador:
+    __puertos: list[PuertoRed] = []
+    __encendido: bool = False
 
-    def __init__(self, puertos: list[PuertoRed]) -> None:
-        self.__puertos == puertos
+    def __init__(self, cantidad_puertos: int) -> None:
+        self.__cantidad_puertos = cantidad_puertos
+
+
+    @property
+    def puertos(self) -> list[PuertoRed]:
+        return self.__puertos
+    
+
+    @property
+    def cantidad_puertos(self) -> int:
+        return self.__cantidad_puertos
+
+
+    @property
+    def encendido(self) -> bool:
+        return self.__encendido
+    
+
+    def encender(self):
+        self.__encendido = True
+
+
+    def apagar(self):
+        self.__encendido = False
+
+
+    def añadir_puerto(self, puerto: PuertoRed):
+        if self.__encendido:
+            raise ConmutadorError('El conmutador está encendido.')
+        elif len(self.__puertos) == self.__cantidad_puertos:
+            raise ConmutadorError('El conmutador está completo.')
+        else:
+            self.__puertos.append(puerto)
+
+
+class ConmutadorError(Exception):
+    ...
--- 02_capa_acceso_medio/TestConmutador.py
+++ 02_capa_acceso_medio/TestConmutador.py
@@ -1,22 +1,86 @@
 import unittest
 
-from Conmutador import Conmutador
+from Conmutador import Conmutador, ConmutadorError
 from DireccionFisica import DireccionFisica
 from PuertoRed import PuertoRed, Tecnologia
+
 
 class TestConmutador(unittest.TestCase):
     
     def test_init(self):
-        direccion1 = DireccionFisica('00:00:00:00:00:01')
-        puerto1 = PuertoRed(direccion1, Tecnologia.PAR_TRENZADO_1000)
-        direccion2 = DireccionFisica('00:00:00:00:00:02')
-        puerto2 = PuertoRed(direccion1, Tecnologia.PAR_TRENZADO_1000)
-        direccion3 = DireccionFisica('00:00:00:00:00:03')
-        puerto3 = PuertoRed(direccion1, Tecnologia.PAR_TRENZADO_1000)
-        direccion4 = DireccionFisica('00:00:00:00:00:04')
-        puerto4 = PuertoRed(direccion1, Tecnologia.PAR_TRENZADO_1000)
-        conmutador = Conmutador([puerto1, puerto2, puerto3, puerto4])
+        conmutador = Conmutador(10)
         self.assertIsInstance(conmutador, Conmutador)
+
+
+    def test_getter_cantidad_puertos(self):
+        conmutador = Conmutador(10)
+        self.assertEqual(conmutador.cantidad_puertos, 10)
+
+
+    def test_getter_puertos(self):
+        conmutador = Conmutador(10)
+        self.assertListEqual(conmutador.puertos, [])
+
+
+    def test_getter_encendido(self):
+        conmutador = Conmutador(10)
+        self.assertFalse(conmutador.encendido)
+
+
+    def test_encender(self):
+        conmutador = Conmutador(10)
+        self.assertFalse(conmutador.encendido)
+
+        conmutador.encender()
+        self.assertTrue(conmutador.encendido)
+
+
+    def test_apagar(self):
+        conmutador = Conmutador(10)
+        self.assertFalse(conmutador.encendido)
+
+        conmutador.encender()
+        conmutador.apagar()
+        self.assertFalse(conmutador.encendido)
+
+
+    def test_añadir_puerto(self):
+        conmutador = Conmutador(4)
+
+        direccion1 = DireccionFisica("AA:AA:AA:AA:AA:AA")
+        puerto1 = PuertoRed(direccion1, Tecnologia.PAR_TRENZADO_1000)
+        conmutador.añadir_puerto(puerto1)
+        self.assertEqual(len(conmutador.puertos), 1)
+        self.assertListEqual(conmutador.puertos, [puerto1])
+
+        conmutador.encender()
+        direccion2 = DireccionFisica("AA:AA:AA:AA:AA:BB")
+        puerto2 = PuertoRed(direccion2, Tecnologia.PAR_TRENZADO_1000)
+        with self.assertRaises(ConmutadorError):
+            conmutador.añadir_puerto(puerto2)
+
+        conmutador.apagar()
+        conmutador.añadir_puerto(puerto2)
+        self.assertEqual(len(conmutador.puertos), 2)
+        self.assertListEqual(conmutador.puertos, [puerto1, puerto2])
+
+        direccion3 = DireccionFisica("AA:AA:AA:AA:AA:33")
+        puerto3 = PuertoRed(direccion3, Tecnologia.PAR_TRENZADO_1000)
+        conmutador.añadir_puerto(puerto3)
+        self.assertEqual(len(conmutador.puertos), 3)
+        self.assertListEqual(conmutador.puertos, [puerto1, puerto2, puerto3])
+
+        direccion4 = DireccionFisica("AA:AA:AA:AA:AA:44")
+        puerto4 = PuertoRed(direccion4, Tecnologia.PAR_TRENZADO_1000)
+        conmutador.añadir_puerto(puerto4)
+        self.assertEqual(len(conmutador.puertos), 4)
+        self.assertListEqual(conmutador.puertos, [puerto1, puerto2, puerto3, puerto4])
+
+        direccion5 = DireccionFisica("AA:AA:AA:AA:AA:55")
+        puerto5 = PuertoRed(direccion5, Tecnologia.PAR_TRENZADO_1000)
+        with self.assertRaises(ConmutadorError):
+            conmutador.añadir_puerto(puerto5)
+
 
 if __name__ == '__main__':
     unittest.main()
