8950018c1c96e857139dc15766c2e2afa5597578
adolfo-martin <adolfo.martin@iesramonarcas.es>
2024-05-06 14:30:37 +0200

    hoy

---
+++ 09_escaneador_puertos/TerminalColors.py
@@ -0,0 +1,21 @@
+class TerminalColors:
+    BLACK_GRAY = '\033[0;30;40m'
+    BLACK_RED = '\033[0;31;40m'
+    BLACK_GREEN = '\033[0;32;40m'
+    BLACK_YELLOW = '\033[0;33;40m'
+    BLACK_BLUE = '\033[0;34;40m'
+    BLACK_MAGENTA = '\033[0;35;40m'
+    BLACK_CYAN = '\033[0;36;40m'
+    BLACK_WHITE = '\033[0;37;40m'
+    WHITE_GRAY = '\033[0;30;47m'
+    WHITE_RED = '\033[0;31;47m'
+    WHITE_GREEN = '\033[0;32;47m'
+    WHITE_YELLOW = '\033[0;33;47m'
+    WHITE_BLUE = '\033[0;34;47m'
+    WHITE_MAGENTA = '\033[0;35;47m'
+    WHITE_CYAN = '\033[0;36;47m'
+    WHITE_WHITE = '\033[0;37;47m'
+
+
+print(TerminalColors.WHITE_BLUE, 'Hola')
+print(TerminalColors.BLACK_WHITE, 'adios')
